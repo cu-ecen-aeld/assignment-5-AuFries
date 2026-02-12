5f3293fe9b9685e98c37912a5eee82829cfcefe2
Aufries <austfries@gmail.com>
2026-02-11 21:08:07 -0600

    Project setup

--- build.sh
+++ build.sh
@@ -12,20 +12,20 @@ git submodule update
 set -e 
 cd `dirname $0`
 
-if [ ! -e buildroot/.config ]
+if [ ! -e buildroot/.config ] # if config does not already exist
 then
 	echo "MISSING BUILDROOT CONFIGURATION FILE"
 
-	if [ -e ${AESD_MODIFIED_DEFCONFIG} ]
+	if [ -e ${AESD_MODIFIED_DEFCONFIG} ] # make external modified defconfig
 	then
 		echo "USING ${AESD_MODIFIED_DEFCONFIG}"
 		make -C buildroot defconfig BR2_EXTERNAL=${EXTERNAL_REL_BUILDROOT} BR2_DEFCONFIG=${AESD_MODIFIED_DEFCONFIG_REL_BUILDROOT}
-	else
+	else # make default QEMU config
 		echo "Run ./save_config.sh to save this as the default configuration in ${AESD_MODIFIED_DEFCONFIG}"
 		echo "Then add packages as needed to complete the installation, re-running ./save_config.sh as needed"
 		make -C buildroot defconfig BR2_EXTERNAL=${EXTERNAL_REL_BUILDROOT} BR2_DEFCONFIG=${AESD_DEFAULT_DEFCONFIG}
 	fi
-else
+else # actually build if config exists
 	echo "USING EXISTING BUILDROOT CONFIG"
 	echo "To force update, delete .config or make changes using make menuconfig and build again."
 	make -C buildroot BR2_EXTERNAL=${EXTERNAL_REL_BUILDROOT}
